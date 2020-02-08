fb4d0adabcab0015f4fbbb685e16bcd76d32c0de
swyroak <947x9i@bma.biglobe.ne.jp>
2020-02-08 22:35:29 +0900

    add slidemenu

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function clickMenu(e) {
     console.log('aaaaa')
+    let slideMenu = document.getElementsByClassName('slide-menu')
+    console.log(slideMenu)
+    slideMenu[0].style.display = 'block'
 }
